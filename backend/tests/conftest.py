4867b8dd9c04ae14dab8cc443b30cf11a2e2d862
Wytheny <wtmarinho55@gmail.com>
2025-05-20 10:12:46 -0300

    feat: add temporary database creation file for testing

---
+++ backend/tests/conftest.py
@@ -0,0 +1,37 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.main import app, get_db
+from app.database import Base
+from app import models
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture()
+def test_db():
+    Base.metadata.create_all(bind=engine)
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        db.close()
+    Base.metadata.drop_all(bind=engine)
+
+@pytest.fixture()
+def client(test_db):
+    def override_get_db():
+        try:
+            yield test_db
+        finally:
+            test_db.close()
+    
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+    app.dependency_overrides.clear()
