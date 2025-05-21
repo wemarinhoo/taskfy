ab34c46488caf6fb87659dcafb1abf29d8bd3a70
Wytheny <wtmarinho55@gmail.com>
2025-05-21 09:35:53 -0300

    feat: added 'update task' schema

--- backend/app/schemas.py
+++ backend/app/schemas.py
@@ -11,3 +11,7 @@ class TaskResponse(TaskBase):
     id: int
     done: bool
     model_config = ConfigDict(from_attributes=True)
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    done: bool | None = None
